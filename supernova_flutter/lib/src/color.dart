998f1573ce201558869a909e455102802352f2c4
Jonas Wanke <contact@jonas-wanke.com>
2023-05-02 09:42:19 +0200

    Add ColorJsonConverter

--- supernova_flutter/lib/src/color.dart
+++ supernova_flutter/lib/src/color.dart
@@ -5,7 +5,20 @@ import 'package:supernova/supernova.dart';
 extension ColorSupernova on Color {
   RgbColor get asRgbColor => RgbColor(red, green, blue);
 
-  static Color fromJson(String json) {
+  static Color fromJson(String json) =>
+      const ColorJsonConverter().fromJson(json);
+  String toJson() => const ColorJsonConverter().toJson(this);
+}
+
+extension RgbColorSupernova on RgbColor {
+  Color get asColor => Color.fromARGB(255, r.toInt(), g.toInt(), b.toInt());
+}
+
+class ColorJsonConverter extends JsonConverter<Color, String> {
+  const ColorJsonConverter();
+
+  @override
+  Color fromJson(String json) {
     assert(json.length == 7);
     assert(json[0] == '#');
     final r = int.parse(json.substring(1, 3), radix: 16);
@@ -14,16 +27,13 @@ extension ColorSupernova on Color {
     return Color.fromARGB(255, r, g, b);
   }
 
-  String toJson() {
+  @override
+  String toJson(Color object) {
     String convertChannel(int channel) =>
         channel.toRadixString(16).padLeft(2, '0');
 
-    return '#${convertChannel(red)}'
-        '${convertChannel(green)}'
-        '${convertChannel(blue)}';
+    return '#${convertChannel(object.red)}'
+        '${convertChannel(object.green)}'
+        '${convertChannel(object.blue)}';
   }
-}
-
-extension RgbColorSupernova on RgbColor {
-  Color get asColor => Color.fromARGB(255, r.toInt(), g.toInt(), b.toInt());
 }

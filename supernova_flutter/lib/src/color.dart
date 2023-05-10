e2b46f2152bc0dbfb8c47bbfa4cd3d3edb9a0e6c
Jonas Wanke <contact@jonas-wanke.com>
2023-05-10 11:16:55 +0200

    Make color component names clearer

--- supernova_flutter/lib/src/color.dart
+++ supernova_flutter/lib/src/color.dart
@@ -21,10 +21,10 @@ class ColorJsonConverter extends JsonConverter<Color, String> {
   Color fromJson(String json) {
     assert(json.length == 7);
     assert(json[0] == '#');
-    final r = int.parse(json.substring(1, 3), radix: 16);
-    final g = int.parse(json.substring(3, 5), radix: 16);
-    final b = int.parse(json.substring(5, 7), radix: 16);
-    return Color.fromARGB(255, r, g, b);
+    final red = int.parse(json.substring(1, 3), radix: 16);
+    final green = int.parse(json.substring(3, 5), radix: 16);
+    final blue = int.parse(json.substring(5, 7), radix: 16);
+    return Color.fromARGB(255, red, green, blue);
   }
 
   @override

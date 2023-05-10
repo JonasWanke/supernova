281e63e305ea6b6f7bb306e2651df00ae0007e3b
Jonas Wanke <contact@jonas-wanke.com>
2023-05-10 11:18:01 +0200

    Add useMenuController()

--- supernova_flutter/lib/src/hooks.dart
+++ supernova_flutter/lib/src/hooks.dart
@@ -1,9 +1,11 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:supernova/supernova.dart';
 
 AutoSizeGroup useAutoSizeGroup() => useMemoized(AutoSizeGroup.new);
+
+MenuController useMenuController() => useMemoized(MenuController.new);
 
 GlobalKey<S> useGlobalKey<S extends State<StatefulWidget>>() =>
     useMemoized(GlobalKey<S>.new);

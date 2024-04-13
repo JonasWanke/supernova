ca294d8a73bd42d598bada095f375aec24866355
Jonas Wanke <contact@jonas-wanke.com>
2024-04-13 20:51:28 +0200

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = with pkgs;
+          mkShell {
+            FLUTTER_ROOT = flutter;
+            buildInputs = [ flutter ];
+          };
+      });
+}

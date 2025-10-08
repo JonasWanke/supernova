295408802dbc43c14b5a33cfdfdd71e1b71a5271
Jonas Wanke <contact@jonas-wanke.com>
2025-10-08 12:44:41 +0200

    Add useDispose(…), useDisposable(…), useOnValueListenableChange(…)

--- supernova_flutter/lib/src/hooks.dart
+++ supernova_flutter/lib/src/hooks.dart
@@ -1,9 +1,22 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:supernova/supernova.dart';
+import 'package:supernova/supernova.dart' hide ValueGetter, ValueSetter;
 
 AutoSizeGroup useAutoSizeGroup() => useMemoized(AutoSizeGroup.new);
+
+void useDispose(VoidCallback callback, [List<Object?> keys = const []]) =>
+    useEffect(() => callback, keys);
+T useDisposable<T>(
+  ValueGetter<T> create, {
+  required void Function(T) dispose,
+  List<Object?> keys = const [],
+}) {
+  final disposable = useMemoized(create, keys);
+  useDispose(() => dispose(disposable));
+  return disposable;
+}
 
 MenuController useMenuController() => useMemoized(MenuController.new);
 
@@ -33,3 +46,8 @@ class RebuildState {
 
   final VoidCallback request;
 }
+
+void useOnValueListenableChange<T>(
+  ValueListenable<T>? listenable,
+  ValueSetter<T> listener,
+) => useOnListenableChange(listenable, () => listener(listenable!.value));

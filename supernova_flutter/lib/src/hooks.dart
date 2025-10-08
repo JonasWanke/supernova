828c18e8e798067da7fb3873bd95a53f705afe9c
Jonas Wanke <contact@jonas-wanke.com>
2025-10-08 12:45:46 +0200

    Add useTickerProvider(…)

--- supernova_flutter/lib/src/hooks.dart
+++ supernova_flutter/lib/src/hooks.dart
@@ -1,6 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:supernova/supernova.dart' hide ValueGetter, ValueSetter;
 
@@ -45,6 +46,55 @@ class RebuildState {
   bool get wasRequestedOrIsFirstBuild => wasRequested || isFirstBuild;
 
   final VoidCallback request;
+}
+
+TickerProvider useTickerProvider({List<Object?>? keys}) {
+  return use(
+    keys != null ? _TickerProviderHook(keys) : const _TickerProviderHook(),
+  );
+}
+
+class _TickerProviderHook extends Hook<TickerProvider> {
+  const _TickerProviderHook([List<Object?>? keys]) : super(keys: keys);
+
+  @override
+  _TickerProviderHookState createState() => _TickerProviderHookState();
+}
+
+class _TickerProviderHookState
+    extends HookState<TickerProvider, _TickerProviderHook>
+    implements TickerProvider {
+  final List<Ticker> _tickers = [];
+
+  @override
+  Ticker createTicker(TickerCallback onTick) {
+    final newTicker = Ticker(onTick, debugLabel: 'created by $context');
+    _tickers.add(newTicker);
+    return newTicker;
+  }
+
+  @override
+  void dispose() {
+    if (_tickers.any((ticker) => ticker.isActive)) {
+      throw FlutterError(
+        'Not all tickers created by useTickerProvider were cleaned up',
+      );
+    }
+  }
+
+  @override
+  TickerProvider build(BuildContext context) {
+    for (final ticker in _tickers) {
+      ticker.muted = !TickerMode.of(context);
+    }
+    return this;
+  }
+
+  @override
+  String get debugLabel => 'useTickerProvider';
+
+  @override
+  bool get debugSkipValue => true;
 }
 
 void useOnValueListenableChange<T>(

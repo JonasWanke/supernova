e3827ae8ba794108360b7792b10ed3f69b8dbd5d
Jonas Wanke <contact@wanke.dev>
2026-04-09 14:24:51 +0200

    Upgrade to Flutter 3.41

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        flutter = pkgs.flutterPackages.v3_38;
+        flutter = pkgs.flutterPackages.v3_41;
       in
       {
         devShell =
